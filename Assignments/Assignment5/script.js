c7ab7afcd1dd09f79dff8f58d1916cff54926803
sockrubbah <solomander81@gmail.com>
2025-02-09 16:12:46 -0500

    he

--- Assignments/Assignment5/script.js
+++ Assignments/Assignment5/script.js
@@ -2,17 +2,17 @@ window.onload = () => {
     function helloFunction() {
         document.getElementById("hello-column").onclick = () => {
             document.getElementById("hello-box").innerHTML += "<p>hello</p>";
-        }
+        };
     }
 
     function colorFunction() {
-        document.getElementByClassName("colorPicker").oninput =(e) => {
+        document.getElementById("colorPicker").oninput = (e) => {
             document.getElementById("star").style.color = e.target.value;
-        }
+        };
     }
 
     function imageFunction() {
-        document.getElementById("toggleImage").onclick = () {
+        document.getElementById("toggleImage").onclick = () => {
 
         }
     }
